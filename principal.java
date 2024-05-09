48d5f853d0c4c91abb22b7d2d9098314f12177f9
Francisco <merveatrece@gmail.com>
2024-05-09 14:07:31 +0200

    Actualizar uno de los ficheros del ejercicio 1 y comprobar su estado en el repositorio.

--- principal.java
+++ principal.java
@@ -3,6 +3,7 @@ public class principal {
 
         String nombre = "Francisco Jose Garcia Vega";
         System.out.println("El creador de esta tarea es: " + nombre);
+        System.out.println("Este cambio va a ser revertido proximamente.");
 
     }
 }
