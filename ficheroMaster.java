d3626160b78b25d1c4733dd776fed60dc0825f08
Francisco <merveatrece@gmail.com>
2024-05-09 15:44:45 +0200

    Cambiar el nuevo fichero.

--- ficheroMaster.java
+++ ficheroMaster.java
@@ -1,5 +1,5 @@
 public class ficheroMaster {
     public static void main(String[] args) {
-        System.out.println("Soy el fichero de la rama main");
+        System.out.println("Soy el fichero de la ramaFran");
     }
 }
