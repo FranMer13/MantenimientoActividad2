5ac1891b8c879b9ddb658491b08846d41eafacab
Francisco <merveatrece@gmail.com>
2024-05-09 13:47:28 +0200

    Crear un fichero y subirlo al repositorio

---
+++ principal.java
@@ -0,0 +1,8 @@
+public class principal {
+    public static void main(String[] args) {
+
+        String nombre = "Fran Garcia Vega";
+        System.out.println("El creador de esta tarea es: " + nombre);
+
+    }
+}
